c00cea153c0db0434887c29b36ee04d10ab8a1d4
Mariusz Derezinski-Choo <mnd14@duke.edu>
2020-02-24 00:03:10 -0500

    added Backwards Node

---
+++ src/ParserModel/Commands/ForwardNode.java
@@ -0,0 +1,32 @@
+package ParserModel.Commands;
+
+import ParserModel.ParserNode;
+
+public class ForwardNode extends ParserNode {
+    private ParserNode myLength;
+
+    public ForwardNode(){
+
+    }
+
+    @Override
+    public double execute() {
+        System.out.println(toString());
+        return 0; //FIXME
+    }
+
+    @Override
+    public int numberOfNodes() {
+        return 1;
+    }
+
+    @Override
+    public void addNode(ParserNode node) {
+        myLength = node;
+    }
+
+    @Override
+    public String toString(){
+        return "Moving forward " + myLength.execute();
+    }
+}
