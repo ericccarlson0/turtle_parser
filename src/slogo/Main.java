5f663e51b8359cb3737ecedd8887602c30f80256
Mariusz Derezinski-Choo <mnd14@duke.edu>
2020-02-23 20:23:23 -0500

    changed main method for testing parser

--- src/slogo/Main.java
+++ src/slogo/Main.java
@@ -9,6 +9,7 @@ public class Main {
     private static final String RESOURCE_PACKAGE = "resources.languages.";
     private static final String LANGUAGE = "English";
     public static final ResourceBundle RESOURCES = ResourceBundle.getBundle(RESOURCE_PACKAGE + LANGUAGE);
+    public static final ResourceBundle SYNTAX = ResourceBundle.getBundle("resources.regex.syntax");
     /**
      * Start of the program.
      */
