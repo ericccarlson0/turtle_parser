81bc9d0c5aa04e9f3ea0ef512e9d0d1713373010
Mariusz Derezinski-Choo <mnd14@duke.edu>
2020-02-27 01:13:34 -0500

    commented/refactored CommandMissingListStartException

--- src/parserModel/exceptions/ParsingException.java
+++ src/parserModel/exceptions/ParsingException.java
@@ -2,6 +2,10 @@ package parserModel.exceptions;
 
 import parserModel.nodes.ParserNode;
 
+import java.util.ResourceBundle;
+
 public abstract class ParsingException extends RuntimeException{
+    protected static final ResourceBundle ERROR_MESSAGE_RESOURCES = ResourceBundle.getBundle("exceptions.resources.English.properties");
+
     public abstract ParserNode toNode();
 }
