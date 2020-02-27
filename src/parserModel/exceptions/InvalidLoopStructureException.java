00566fb1732f6fa3728c209723ea8037e69c9111
Mariusz Derezinski-Choo <mnd14@duke.edu>
2020-02-27 01:22:31 -0500

    refactored InvalidLoopStructureException

--- src/parserModel/exceptions/InvalidLoopStructureException.java
+++ src/parserModel/exceptions/InvalidLoopStructureException.java
@@ -3,10 +3,22 @@ package parserModel.exceptions;
 import parserModel.nodes.ParserNode;
 import parserModel.nodes.errorNodes.ErrorNode;
 
+/**
+ * An Exception that represents when a Loop structure is Invalid
+ * this occurs because the loop does not have a list as a header that
+ * specifies a variable and increments
+ *
+ * @author Mariusz Derezinski-Choo
+ */
 public class InvalidLoopStructureException extends ParsingException{
+    private static final String ERROR_MESSAGE_KEY = "InvalidLoopStructure";
 
+    /**
+     * fetch a ParserNode that can be executed in response to the exception
+     * @return a ParserNode that can be executed in response to the exception
+     */
     @Override
     public ParserNode toNode() {
-        return new ErrorNode("Error: Missing Open Bracket at start of loop header");
+        return new ErrorNode(ERROR_MESSAGE_RESOURCES.getString(ERROR_MESSAGE_KEY));
     }
 }
