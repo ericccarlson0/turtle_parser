f83eda3a76f1031b835f339ce07e8cb803957a56
Mariusz Derezinski-Choo <mnd14@duke.edu>
2020-02-27 00:23:46 -0500

    commented parsingException

--- src/parserModel/exceptions/ParsingException.java
+++ src/parserModel/exceptions/ParsingException.java
@@ -2,6 +2,16 @@ package parserModel.exceptions;
 
 import parserModel.nodes.ParserNode;
 
+/**
+ * An abstract superclass that defines the behavior of an Exception that occurs during parsing.
+ *
+ * @author Mariusz Derezinski-Choo
+ */
 public abstract class ParsingException extends RuntimeException{
+    /**
+     * retrieve a ParserNode that corresponds to the actions that should be taken in response to this Exception
+     *
+     * @return a ParserNode that is to be executed in response to the Exception
+     */
     public abstract ParserNode toNode();
 }
