3fc668d94e3cda507ebf353a749f63a3fb0e09fe
Lorne Zhang <lz150@duke.edu>
2020-03-02 17:18:06 -0500

    Allowed user to click a command in the history to replay the action. Also added FD BK RT LT buttons to allow the user to hold perform the action for however long the button is held down. Also created SplashScreen

--- src/slogo/Main.java
+++ src/slogo/Main.java
@@ -1,5 +1,15 @@
 package slogo;
 
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import visualizer.Controller;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -7,21 +17,76 @@ import java.io.IOException;
 import java.util.*;
 
 public class Main extends Application {
+    private static final int SPLASH_WIDTH = 500;
+    private static final int SPLASH_HEIGHT = 500;
+    private static final String RESOURCE_FOLDER = "/stylesheets";
+    private static final String STYLESHEET = "/default.css";
 
-     public static void main (String[] args) {
+    public static void main (String[] args) {
         launch(args);
     }
 
     public void start(Stage stage) throws IOException {
-        Controller test = new Controller();
+        initiate();
+        //Controller test = new Controller();
     }
-}
 
-/**
- * Scanner kbReader = new Scanner(System.in);
- *         TreeParser parser = new TreeParser(new ArrayList<>());
- *         while(true){
- *             CommandParserNode node = parser.parseString(kbReader.nextLine());
- *             node.execute();
- *         }
- */
+    private void initiate() {
+        Stage splashStage = new Stage();
+        Scene splashScene = createSplashScene();
+        String stylesheet = String.format("%s%s", RESOURCE_FOLDER, STYLESHEET);
+        splashScene.getStylesheets().add(getClass().getResource(stylesheet).toExternalForm());
+        splashStage.setScene(splashScene);
+        splashStage.show();
+    }
+
+    private Scene createSplashScene() {
+        Group splashGroup = new Group();
+        BorderPane splashBorderPane = new BorderPane();
+
+        Node splashButtons = createWorkSpaceButtons();
+        splashBorderPane.setCenter(splashButtons);
+        BorderPane.setAlignment(splashButtons, Pos.CENTER);
+
+        Node splashWelcome = createWelcomeText();
+        splashBorderPane.setTop(splashWelcome);
+        BorderPane.setAlignment(splashWelcome, Pos.CENTER);
+
+        Node splashInformation = createInformationText();
+        splashBorderPane.setBottom(splashInformation);
+        BorderPane.setAlignment(splashInformation, Pos.CENTER);
+
+        splashGroup.getChildren().add(splashBorderPane);
+        Scene splash = new Scene(splashGroup, SPLASH_WIDTH, SPLASH_HEIGHT);
+        return splash;
+    }
+
+    private Node createInformationText() {
+        VBox information = new VBox();
+        Text title = new Text("Welcome To SLOGO by Mariuz, Eric, Cemal and Lorne");
+        Text instructions = new Text("Press the Work Space button to start!");
+        information.getChildren().add(title);
+        information.getChildren().add(instructions);
+        return information;
+    }
+
+    private Node createWorkSpaceButtons() {
+        VBox buttons = new VBox();
+        Button workSpace = new Button ("Work Space");
+        workSpace.setOnAction(e -> createController());
+        buttons.getChildren().add(workSpace);
+        return buttons;
+    }
+
+    private void createController() {
+        Controller newController = new Controller();
+    }
+
+    private Node createWelcomeText() {
+        HBox welcome = new HBox();
+        Text title = new Text("SLOGO");
+        welcome.getChildren().add(title);
+        return welcome;
+    }
+
+}
