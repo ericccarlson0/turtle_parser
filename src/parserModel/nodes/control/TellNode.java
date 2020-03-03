4a06fe54eed1ab13c1876b7ff39c40e9c8b92c3c
Mariusz Derezinski-Choo <mnd14@duke.edu>
2020-03-03 02:12:49 -0500

    refactored control nodes to account for non-singleton global data

--- src/parserModel/nodes/control/TellNode.java
+++ src/parserModel/nodes/control/TellNode.java
@@ -1,4 +1,23 @@
 package parserModel.nodes.control;
 
-public class TellNode {
+import parserModel.TurtleContext;
+import parserModel.nodes.BinaryOperationNode;
+import parserModel.nodes.ParserNode;
+import parserModel.nodes.UnaryOperationNode;
+
+import java.util.List;
+
+public class TellNode extends BinaryOperationNode {
+
+
+    @Override
+    public double execute(TurtleContext context) {
+        context.clearActiveTurtles();
+        double turtles = operand1.execute(context);
+        for(double i = 0; i < turtles; i++){
+            context.getData().createTurtle(i);
+            context.addActiveTurtles(List.of(i));
+        }
+        return  operand2.execute(context);
+    }
 }
