fec5a7013094ba890d590340baadf5ba72b416c4
Mariusz Derezinski-Choo <mnd14@duke.edu>
2020-03-03 02:12:18 -0500

    refactored turtleQueries to remove reliance on singleton Global Data

--- src/parserModel/nodes/turtleQueries/HeadingNode.java
+++ src/parserModel/nodes/turtleQueries/HeadingNode.java
@@ -6,6 +6,8 @@ import parserModel.nodes.CommandParserNode;
 import parserModel.nodes.ParserNode;
 import parserModel.TurtleContext;
 
+import java.util.List;
+
 public class HeadingNode extends CommandParserNode {
 
   public void addNode(ParserNode node) {
@@ -13,7 +15,9 @@ public class HeadingNode extends CommandParserNode {
   }
 
   public double execute(TurtleContext context) {
-    TurtleData td = GlobalData.getInstance().turtleData();
+    // get the heading of the LAST turtle
+    List<Double> turtles = context.getActiveTurtles();
+    TurtleData td = context.getData().turtleData(turtles.get(turtles.size()-1));
     return td.getHeading();
   }
 
