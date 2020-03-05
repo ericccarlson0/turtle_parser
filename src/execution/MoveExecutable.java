e1ba81a623af6ef09e8923a714de15da55d214e0
Cemal Yagcioglu <cy111@duke.edu>
2020-03-05 16:21:34 -0500

    removed unused imports from HideExecutable and MoveExecutable

--- src/execution/MoveExecutable.java
+++ src/execution/MoveExecutable.java
@@ -1,11 +1,7 @@
 package execution;
 
-import javafx.beans.property.StringProperty;
-import parserModel.TurtleContext;
 import visualizer.Visualizer;
-
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 /**
