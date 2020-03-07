d8a309c09d698ab1f35c74539be00ce16edcc052
Mariusz Derezinski-Choo <mnd14@duke.edu>
2020-03-07 01:37:25 -0500

    refactored CommandFactory to catch exceptions properly

---
+++ src/parserModel/exceptions/UnexpectedParsingException.java
@@ -0,0 +1,11 @@
+package parserModel.exceptions;
+
+import parserModel.nodes.ParserNode;
+
+public class UnexpectedParsingException extends ParsingException {
+
+    @Override
+    public ParserNode renderNode() {
+        return null //FIXME!
+    }
+}
--- src/parserModel/nodes/CommandFactory.java
+++ src/parserModel/nodes/CommandFactory.java
@@ -2,15 +2,20 @@ package parserModel.nodes;
 
 import parserModel.TurtleContext;
 import parserModel.exceptions.NoSuchCommandException;
+import parserModel.exceptions.UnexpectedParsingException;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Iterator;
 import java.util.ResourceBundle;
 
-
+/**
+ * A class that abstracts the implementation of creating a Command Node using reflection
+ */
 public class CommandFactory {
     private static final String RESOURCE_PATH = "parserModel.nodes.CommandNodeReflection";
     private static final ResourceBundle commandNameResource = ResourceBundle.getBundle(RESOURCE_PATH);
+    private static final String ROOT_NODE_PACKAGE = "parserModel.nodes.";
     /**
      * This is a huge case statement that turns 'identifier' strings into various instances of
      * ParserNode sub-classes
@@ -19,18 +24,15 @@ public class CommandFactory {
      */
     public ParserNode createCommand(String identifier, String textEntered, TurtleContext context) {
         debug();
-        if (commandNameResource.containsKey(identifier)) {
-            try {
-                String NodeClassPath = "parserModel.nodes."+ commandNameResource.getString(identifier);
-                Constructor<?> constructor = Class.forName(NodeClassPath).getConstructor(String.class);
-                return (ParserNode) constructor.newInstance(textEntered);
-            } catch (Exception e) {
-                e.printStackTrace(); //TODO: return errorNode.
-            }
-        }
+        String NodeClassPath = ROOT_NODE_PACKAGE + commandNameResource.getString(identifier);
         try {
+            Constructor<?> constructor = Class.forName(NodeClassPath).getConstructor(String.class);
+            return (ParserNode) constructor.newInstance(textEntered);
+        } catch (InstantiationException | InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+            throw new UnexpectedParsingException();
+        } catch (ClassNotFoundException e) {
             return context.getData().getCommand(identifier);
-        } catch (NoSuchCommandException e){
+        } catch(NoSuchCommandException e){
             return new WildCardToken(identifier);
         }
     }
